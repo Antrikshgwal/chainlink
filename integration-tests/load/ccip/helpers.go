3660f1d0946356089c955097a5d5841e1b6bdf61
0xAustinWang <austiwang+eth@gmail.com>
2025-06-09 14:48:21 -0700

    bump funding amount

--- integration-tests/load/ccip/helpers.go
+++ integration-tests/load/ccip/helpers.go
@@ -46,7 +46,7 @@ const (
 )
 
 var (
-	fundingAmount = new(big.Int).Mul(deployment.UBigInt(100), deployment.UBigInt(1e18)) // 100 eth
+	fundingAmount = new(big.Int).Mul(deployment.UBigInt(1e5), deployment.UBigInt(1e18)) // 100 eth
 )
 
 type finalSeqNrReport struct {
